7f68560e0185120738b04f93791ffe791ea94d1c
Jose Espinoza <espinozajgx@gmail.com>
2021-03-28 00:56:42 -0300

    myhotel

--- src/main/java/com/myhotel/demo/DemoApplication.java
+++ src/main/java/com/myhotel/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.acid.demo;
+package com.myhotel.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/myhotel/demo/DemoApplicationTests.java
+++ src/test/java/com/myhotel/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.acid.demo;
+package com.myhotel.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
